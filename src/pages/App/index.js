0b58a553b3e6623351788f43347e0321e10f2f9c
silvanoamaral <silvano.amaral9@gmail.com>
2020-07-15 21:15:19 -0300

    feat: obtendo dados no compoment

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import Header from '../../components/Header'
 
 import '../../assets/css/index.scss'
 
+const loadingInitial = {
+  data: [],
+  loading: false,
+  error: null
+}
+
 const App = () => {
+  const [result, setResult] = useState(loadingInitial)
+
+  useEffect(() => {
+    setResult({
+      ...result,
+      loading: true
+    })
+
+    const getData = async () => {
+      const response = await axios.get('/api/obterDados')
+
+      setResult({
+        ...result,
+        data: response.data,
+        loading: false
+      })
+    }
+
+    getData()
+  }, [setResult])
+
   return (
     <>
       <Header />
